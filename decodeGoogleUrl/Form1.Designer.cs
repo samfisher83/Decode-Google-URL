ad87b8092d4bacf6969a28651261982d8a33633c
samfisher83 <ram_80@hotmail.com>
2012-02-17 11:05:06 -0600

    2nd commit

---
+++ decodeGoogleUrl/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(302, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(302, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 106);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Decode Url";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ decodeGoogleUrl/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Web;
+using ClipboardAssist;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        ClipboardMonitor monitor;
+        public Form1()
+        {   
+            InitializeComponent();
+            monitor = new ClipboardMonitor();
+            monitor.ClipboardChanged += new EventHandler<ClipboardChangedEventArgs>(monitor_ClipboardChanged);
+            
+        }
+
+        void monitor_ClipboardChanged(object sender, ClipboardChangedEventArgs e)
+        {
+            if(Clipboard.GetText().Contains("encrypted.google.com")){
+                textBox1.Text = Clipboard.GetText();
+                FindUrl();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            FindUrl();
+        }
+
+        private void FindUrl()
+        {
+            Regex regex = new Regex("url=(.*?)&");
+            Match match = regex.Match(textBox1.Text);
+            if (match.Groups.Count > 1)
+            {
+                textBox2.Text = Uri.UnescapeDataString(Regex.Unescape(match.Groups[1].Value)).Substring(7);
+                if (textBox2.Text[0] == '/')
+                    textBox2.Text = textBox2.Text.Substring(1);
+
+                Clipboard.SetText(textBox2.Text);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text = Clipboard.GetText();
+            FindUrl();
+
+        }
+    }
+}
